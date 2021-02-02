60d7a495596dc9f3564fe71c82f878d0c2cea008
Sanghyo <sanghyoboy@naver.com>
2021-02-02 14:25:52 +0900

    add some comment to function and print value

--- stack_array.py
+++ stack_array.py
@@ -11,6 +11,7 @@ class Stack(object):
         return self.top == self.capacity - 1
 
     def push(self, value):
+        # stack이 array의 기존 사이즈만큼 꽉 찼을 경우 array 크기 2배로 조정
         if self.isFull():
             tmp = [0 for _ in range(self.capacity)]
             self.array += tmp
@@ -19,6 +20,7 @@ class Stack(object):
         self.top += 1
         self.array[self.top] = value
 
+    # stack에서 top이 가리키고 있는 값 뽑아냄
     def pop(self):
         if self.isEmpty():
             return -1
@@ -26,6 +28,7 @@ class Stack(object):
             self.top -= 1
             return self.array[self.top + 1]
 
+    # stack에서 top이 가리키고 있는 값 출력, pop과는 다름
     def peek(self):
         if self.isEmpty():
             return -1
@@ -46,20 +49,20 @@ class Stack(object):
 
 s = Stack(5)
 s.push(2)
-s.showStack()
+s.showStack()  # >>> 2
 s.push(3)
-s.showStack()
+s.showStack()  # >>> 2 3
 s.push(2)
-s.showStack()
+s.showStack()  # >>> 2 3 2
 s.push(3)
-s.showStack()
+s.showStack()  # >>> 2 3 2 3
 s.push(2)
-s.showStack()
+s.showStack()  # >>> 2 3 2 3 2
 s.push(3)
-s.showStack()
+s.showStack()  # >>> 2 3 2 3 2 3
 s.pop()
-s.showStack()
-print("stack의 맨 위에 있는 값 : ", s.peek())
+s.showStack()  # >>> 2 3 2 3 2
+print("stack의 맨 위에 있는 값 : ", s.peek())  # >>> stack의 맨 위에 있는 값 : 2
 s.pop()
-s.showStack()
-print("stack의 맨 위에 있는 값 : ", s.peek())
+s.showStack()  # >>> 2 3 2 3
+print("stack의 맨 위에 있는 값 : ", s.peek())  # >>> stack의 맨 위에 있는 값 : 3
